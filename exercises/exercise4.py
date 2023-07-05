1afe471d53e0b88e6cccdad653107b3c1df4490f
aleemfau <aleemudcs@gmail.com>
2023-07-05 15:23:21 +0200

    Excercise_4.py

--- exercises/exercise4.py
+++ exercises/exercise4.py
@@ -4,21 +4,32 @@ import urllib.request
 import numpy as np
 from sqlalchemy import create_engine
 
-# Fetching
+#               ***Data Fetching***
 url = "https://www.mowesta.com/data/measure/mowesta-dataset-20221107.zip"
 path = "./exercises/mowesta-dataset-20221107.zip"
 urllib.request.urlretrieve(url, path)
 
-# Extracting
+#               ***Extracting***
 with zipfile.ZipFile(path, 'r') as zip_ref:
     zip_ref.extractall('./exercises')
 
-# Reading
-df = pd.read_csv("./exercises/data.csv", delimiter=';', index_col=False, usecols=["Geraet", "Hersteller", "Model", "Monat", "Temperatur in °C (DWD)", "Batterietemperatur in °C", "Geraet aktiv"])
 
-# Columns Renaming
+#               ***Data Reading***
+df = pd.read_csv("./exercises/data.csv", delimiter=";", decimal=",", index_col=False, 
+                 usecols=["Geraet", "Hersteller", "Model", "Monat", "Temperatur in °C (DWD)", "Batterietemperatur in °C", "Geraet aktiv"])
+
+#               ***Columns Renaming***
 df = df.rename(columns={"Temperatur in °C (DWD)": "Temperatur", "Batterietemperatur in °C": "Batterietemperatur"})
-# Types Validation
+
+#               ***Data Transformation (C To F)***
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+# Convert temperatures to Fahrenheit
+df["Temperatur"] = celsius_to_fahrenheit(df["Temperatur"])
+df["Batterietemperatur"] = celsius_to_fahrenheit(df["Batterietemperatur"])
+
+#               ***Validation***
 column_types = {
     'Geraet': int,
     'Hersteller': str,
@@ -30,9 +41,5 @@ column_types = {
 }
 df = df.astype(column_types)
 
-# Convert temperatures to Fahrenheit
-df["Temperatur"] = df["Temperatur"] * 9/5 + 32
-df["Batterietemperatur"] = df["Batterietemperatur"] * 9/5 + 32
-
 # Write to SQLite database
-df.to_sql("temperatures", 'sqlite:///temperatures.sqlite', if_exists="replace", index=False)
+df.to_sql('temperatures', 'sqlite:///temperatures.sqlite', if_exists='replace', index=False)
