a96c8cbf5272b9a6ac3eb293f2080abf708186fc
aleemfau <aleemudcs@gmail.com>
2023-07-05 14:35:38 +0200

    Excercise_4.py

---
+++ exercises/exercise4.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import zipfile
+import urllib.request
+
+#               ***Fetching***          
+url = "https://www.mowesta.com/data/measure/mowesta-dataset-20221107.zip"
+path = "./exercises/mowesta-dataset-20221107.zip"    
+urllib.request.urlretrieve(url, path)
+
+#               ***Extracting***        
+with zipfile.ZipFile(path, 'r') as zip_ref:
+    zip_ref.extractall('./exercises')
+
+#               ***Reading***           
+
+
+df = pd.read_csv("./exercises/data.csv", delimiter=';', index_col=False, usecols=["Geraet", "Hersteller", "Model", "Monat", "Temperatur in °C (DWD)", "Batterietemperatur in °C", "Geraet aktiv"])
+
+#               ***Columns Renaming***      
+df = df.rename(columns={"Temperatur in °C (DWD)": "Temperatur", "Batterietemperatur in °C": "Batterietemperatur"})
+
+#               ***Data Transformation***
+df["Temperatur"] = df["Temperatur"] * 9/5 + 32
+df["Batterietemperatur"] = df["Batterietemperatur"] * 9/5 + 32
+
+#               ***Types Validation***
+column_types = {
+        'Geraet': int,
+        'Hersteller': str,
+        'Model': str,
+        'Monat': int,
+        'Temperatur': float,
+        'Batterietemperatur': float,
+        'Geraet aktiv': str
+        }
+df = df.astype(column_types)
+
+# write to sqlite database
+
+df.to_sql("temperatures", 'sqlite:///temperatures.sqlite', if_exists="replace", index=False)
+
